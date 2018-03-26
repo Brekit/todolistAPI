2489f9b31927f61352934d8c587f9e55420cd847
brekit <cearth6@gmail.com>
2018-03-25 19:36:28 -0500

    Initial commit

---
+++ api/models/todoListModel.js
@@ -0,0 +1,23 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.schema;
+
+var TaskSchema = new Schema({
+	name: {
+		type: String,
+		required: 'Please enter the name of the task'
+	},
+	Created_date{
+		type: Date,
+		default: Date.now
+	},
+	status: {
+		type: [{
+			type: String,
+			enum: ['Pending', 'ongoing', 'completed']
+		}],
+	default: ['pending']
+			}
+	});
+
+module.exports = mongoose.model('Tasks', TaskSchema);
